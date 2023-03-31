4f51713f878317afcd1439ef1559971e9e9f930a
Ajay Ganesh Suryawanshi <ajaysu@Cybage.com>
2023-03-31 12:41:31 +0530

    Bug Fixing

--- movie-ticket-booking-system/src/main/java/com/movie/serviceImplementation/TheatreServiceImplementation.java
+++ movie-ticket-booking-system/src/main/java/com/movie/serviceImplementation/TheatreServiceImplementation.java
@@ -34,32 +34,35 @@ public class TheatreServiceImplementation implements TheaterService {
 
 	@Override
 	public List<NearByTheaterResponse> getNearByTheater(Double latitude, Double longitude, Double radius) {
-		NearByTheaterResponse nearByTheaterResponse = new NearByTheaterResponse();
+
 		List<TheaterResponse> theaterResponse = this.theaterRepository.findAll().stream()
 				.map(source -> this.modelMapper.map(source, TheaterResponse.class)).toList();
-		double radiusInKm = radius * 1000;
 		List<NearByTheaterResponse> nearByTheaterResponseList = new ArrayList<>();
-
+		//long startTime = System.nanoTime();
 		for (TheaterResponse resposne : theaterResponse) {
 			double theaterDistanceFromUser = distance(latitude, longitude, resposne.getLatitude(),
 					resposne.getLongitude());
 			System.out.println("Distance from me:" + theaterDistanceFromUser);
-			if (theaterDistanceFromUser <= radiusInKm) {
-				nearByTheaterResponse.setCustomMessage("Future scope: 'On going movie(movie name from this theater)' just 'time(in mins ex. 7 mins.)' away from you");
-				// Rounding distance till 1 precision then converting into String with km as
-				// unit
-				StringBuilder dis = new StringBuilder(Double.toString(Math.round(theaterDistanceFromUser * 10) / 10));
+			if (theaterDistanceFromUser <= radius) {
+				NearByTheaterResponse nearByTheaterResponse = new NearByTheaterResponse();
+				nearByTheaterResponse.setCustomMessage(
+						"Future scope: 'On going movie(movie name from this theater)' just 'time(in mins ex. 7 mins.)' away from you");
+				StringBuilder dis = new StringBuilder(
+						Double.toString(Math.round(theaterDistanceFromUser * 10.0) / 10.0));
 				dis.append(" km");
 				/*
-				 * String to StringBuilder using append() method StringBuilder to String
-				 * usingtoString() method
+				 * String to StringBuilder using append() method ...and StringBuilder to String
+				 * using toString() method
 				 */
 				nearByTheaterResponse.setDistanceFromUser(dis.toString());
 				nearByTheaterResponse.setTheterResponse(resposne);
 				nearByTheaterResponseList.add(nearByTheaterResponse);
+				System.out.println("Distances from me (in):" + theaterDistanceFromUser);
 			}
 
 		}
+		
+		//System.out.println("Execution time: "+(System.nanoTime() - startTime)+" nanoseconds");
 
 		return nearByTheaterResponseList;
 	}
@@ -79,6 +82,7 @@ public class TheatreServiceImplementation implements TheaterService {
 	}
 
 	// helper method to calulate the distance
+	// we can use the Distance matrix api of google here for more accuracy.
 	private double distance(double lat1, double lon1, double lat2, double lon2) {
 		double R = 6371; // Radius of the earth in km
 		double dLat = Math.toRadians(lat2 - lat1);
