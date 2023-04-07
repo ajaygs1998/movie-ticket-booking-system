1718b7181f6cbf4991a29aa6cf22be0549c6fa15
Ajay Ganesh Suryawanshi <ajaysu@Cybage.com>
2023-04-07 19:24:57 +0530

    constansts class for the system:

---
+++ movie-ticket-booking-system/src/main/java/com/movie/configurations/SystemConstants.java
@@ -0,0 +1,10 @@
+package com.movie.configurations;
+
+public class SystemConstants {
+	 // Private constructor to prevent instantiation of this class
+	private SystemConstants() {
+       
+    }
+	public  static final int MIN_RATING = 1;
+	public static final int MAX_RATING = 10;
+}
